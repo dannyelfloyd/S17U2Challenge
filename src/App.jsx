4b1fad670adbba75cf4811a7f72cbe9460adb105
dannyelfloyd <dannyelfloyd@gmail.com>
2025-07-07 16:21:51 +0200

    Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import './App.css'
+import './App.css';
+
+import React, { useState, useEffect } from 'react'
+import Task from './components/Task';
+import AddTaskForm from './components/AddTaskForm';
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -7,10 +11,40 @@ const App = () => {
     { id: 3, text: 'Hacer ejercicio', completed: false }
   ]);
 
-  
+  const addTask = (text) => {
+    const newTask = {
+      id: tasks.length + 1,
+      text: text,
+      completed: false
+    }
+    setTasks([...tasks, newTask]);
+  }
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  }
+
+  const toggleTask = (taskId) => {
+    setTasks(tasks.map(task => 
+      task.id === taskId 
+      ? { ...task, completed: !task.completed }
+      : task))
+  }
 
   return (
-    <></>
+    <>
+      <AddTaskForm onAdd={addTask}/>
+      <ul>
+        {tasks.map(task => (
+        <Task 
+          task={task} 
+          key={task.id}
+          onDelete={deleteTask}
+          onToggle={toggleTask}
+        />
+        ))}
+     </ul>
+    </>
   );
 };
 
